2f55c38d909892810c8e98d10538f814cd38465a
mrozinskikj <mrozinskikj@gmail.com>
2024-09-09 18:08:39 +0100

    updated test_utils to work with tensors
    Previously worked with np arrays.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,20 +5,20 @@ from nlp_engineer_assignment import count_letters, score
 
 
 def test_count_letters():
-    assert np.array_equal(count_letters("hello"), np.array([0, 0, 0, 1, 0]))
-    assert np.array_equal(count_letters("world"), np.array([0, 0, 0, 0, 0]))
-    assert np.array_equal(
+    assert torch.equal(count_letters("hello"), torch.tensor([0, 0, 0, 1, 0]))
+    assert torch.equal(count_letters("world"), torch.tensor([0, 0, 0, 0, 0]))
+    assert torch.equal(
         count_letters("hello hello"),
-        np.array([0, 0, 0, 1, 0, 0, 1, 1, 2, 2, 1])
+        torch.tensor([0, 0, 0, 1, 0, 0, 1, 1, 2, 2, 1])
     )
 
 
 def test_score():
-    assert score(np.array([[0, 1, 1, 0, 1]]),
-                 np.array([[0, 1, 1, 0, 1]])) == 1.0
-    assert score(np.array([[0, 1, 1, 0, 1]]),
-                 np.array([[1, 1, 0, 0, 1]])) == 0.6
-    assert score(np.array([[0, 1, 1, 0, 1]]),
-                 np.array([[0, 0, 0, 0, 0]])) == 0.4
-    assert score(np.array([[0, 1, 1, 0, 1]]),
-                 np.array([[1, 0, 0, 1, 0]])) == 0.0
+    assert score(torch.tensor([[0, 1, 1, 0, 1]]),
+                 torch.tensor([[0, 1, 1, 0, 1]])) == 1.0
+    assert score(torch.tensor([[0, 1, 1, 0, 1]]),
+                 torch.tensor([[1, 1, 0, 0, 1]])) == 0.6
+    assert score(torch.tensor([[0, 1, 1, 0, 1]]),
+                 torch.tensor([[0, 0, 0, 0, 0]])) == 0.4
+    assert score(torch.tensor([[0, 1, 1, 0, 1]]),
+                 torch.tensor([[1, 0, 0, 1, 0]])) == 0.0
