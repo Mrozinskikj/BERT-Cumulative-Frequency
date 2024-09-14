c1c6eead47de568b8c50f5ae802446962f141daf
mrozinskikj <mrozinskikj@gmail.com>
2024-09-14 20:03:18 +0100

    api.py docstrings and commenting

--- src/nlp_engineer_assignment/api.py
+++ src/nlp_engineer_assignment/api.py
@@ -3,41 +3,28 @@ from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
 from nlp_engineer_assignment import Tokeniser, predict
-import torch
-
-def process_prediction(
-    input_text: str,
-    model: 'BERT'
-) -> str:
-    """
-    Prepare the input string for the model, get the model prediction, and convert the prediction tensor to a string.
-
-    Parameters
-    ----------
-    input_text : str
-        The input string from the API request.
-    model : BERT
-        The BERT model for computing predictions with.
-    
-    Returns
-    -------
-    str
-        The model prediction converted into a string for API output.
-    """
-    tokeniser = Tokeniser()
-    input_ids = tokeniser.encode(input_text)
-
-    prediction_tensor = predict(input_ids, model)
-    prediction_string = ''.join(str(i.item()) for i in prediction_tensor[0])
-    return prediction_string
 
 
 def create_app(model):
-    print(model)
+    """
+    Create and configure an instance of the FastAPI application.
+
+    Parameters
+    ----------
+    model : BERT
+        The BERT model for computing predictions with.
+
+    Returns
+    -------
+    FastAPI
+        An instance of the configured FastAPI application.
+    """
     app = FastAPI(
         title="BERT Cumulative Frequency",
         version="1.0.0"
     )
+
+    tokeniser = Tokeniser() # create an instance of the tokeniser for input processing
 
     @app.get("/", include_in_schema=False)
     async def index():
@@ -47,15 +34,39 @@ def create_app(model):
         return RedirectResponse(url="/docs")
 
     class TextInput(BaseModel):
+        """
+        Schema for the input text received by the API.
+        """
         text: str
 
     @app.post("/")
     async def serve(input: TextInput) -> dict:
+        """
+        Endpoint to process input text and return model prediction.
+
+        Parameters
+        ----------
+        input : TextInput
+            The input data containing the text to be classified.
+
+        Returns
+        -------
+        dict
+            A dictionary containing the model prediction.
+
+        Raises
+        ------
+        HTTPException
+            If there is a ValueError when unpermitted string is processed by the tokeniser.
+        """
         input_text = input.text
         try:
-            prediction = process_prediction(input_text, model)
+            input_ids = tokeniser.encode(input_text) # encode the input string into tokens
         except ValueError as e:
             raise HTTPException(status_code=400, detail=str(e))
+        
+        prediction_tensor = predict(input_ids, model) # get model predictions from input
+        prediction = ''.join(str(i.item()) for i in prediction_tensor[0]) # convert tensor into string
         return {"prediction": prediction}
     
     return app
